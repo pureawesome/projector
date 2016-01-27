a8346e127b6720dae614ab6b5ade6d4d9b2bb6ca
Gennaro Avolio-Toly <gennaro.the.younger@gmail.com>
2016-01-27 17:33:17 +0000

    Add users and users test

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
  resources :projects, only: [:index]
+ resources :users, only: [:index]
  root to: 'home#index'
 end
---
+++ spec/features/list_users_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.feature 'Listing all users' do
+  scenario 'returns a message when there are no users to view' do
+    visit '/'
+    click_link 'Users'
+        
+    expect(page.current_url).to eq(users_url)
+    expect(page).to have_content('0 users')
+  end
+end
