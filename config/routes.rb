27a3d7254e42785ca46e47b0ce238de4562cb067
Gennaro Avolio-Toly <gennaro.the.younger@gmail.com>
2016-01-28 01:07:32 +0000

    Started experimenting with nested resource routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
- resources :projects, only: [:index]
- resources :users, only: [:index]
- root to: 'home#index'
+  resources :projects, only: [:index] do
+    resources :tasks, only: [:index]
+  end
+  resources :users, only: [:index]
+  root to: 'home#index'
 end
---
+++ spec/features/list_project_tasks_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.feature 'Listing all projects' do
+  scenario 'returns a message when there are no projects to view' do
+    visit '/'
+    click_link 'Projects'
+        
+    expect(page.current_url).to eq(projects_url)
+    expect(page).to have_content('0 projects')
+    
+    click_link 'Tasks'
+    expect(page.current_url).to eq(project_tasks_url)
+    expect(page).to have_content('0 tasks')
+  end
+end
