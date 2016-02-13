11d233c2e04ed9ac539ea5ee6e7e85059bccc905
Bartok <bartok@Bartoks-MacBook-Pro.local>
2016-02-12 19:00:14 -0800

    General show page niceness

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   resources :projects, only: [:index, :new, :create, :show, :edit, :update] do
-    resources :tasks, only: [:index, :new, :create, :show]
+    resources :tasks, only: [:index, :new, :create, :show, :edit, :update]
   end
   resources :users, only: [:index, :new, :create, :show]
+  resources :resources, only: [:index]
   root to: 'home#index'
 end
