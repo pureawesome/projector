722fcfdbd7525276c88cc582daeea753d7e90a6b
Bartok <bartok@Bartoks-MacBook-Pro.local>
2016-03-08 22:16:12 -0800

    More theming and index updates

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,8 @@ class HomeController < ApplicationController
   def index
     @upcoming_tasks = Task.where("due_date > :today", today: DateTime.now).order("due_date ASC").limit(6)
     @recent_tasks = Task.where("status = 0").order("updated_at DESC").limit(6)
-    @projects_by_tasks = Task.pluck('project_id').inject(Hash.new(0)) { |h,v| h[v] += 1; h }.max_by(6) {|k,v| v}
+    @projects_by_tasks = []
+    Task.pluck('project_id').inject(Hash.new(0)) { |h,v| h[v] += 1; h }.max_by(6) {|k,v| v}.each {|k,v| @projects_by_tasks << Project.find(k)}
+    @bookings_upcoming = Booking.where("start_datetime > :now", now: DateTime.now).order("start_datetime ASC").limit(6)
   end
 end
