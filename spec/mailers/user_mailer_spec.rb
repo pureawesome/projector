0ba9cec4b9e14012bf7e01392e9c2b060a94a051
Bartok <bartok@Bartoks-MacBook-Pro.local>
2016-03-09 19:58:22 -0800

    Fixed the mailer tests

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -2,11 +2,13 @@ require "rails_helper"
 
 RSpec.describe UserMailer, type: :mailer do
   describe "thank_you" do
-    let(:mail) { UserMailer.thank_you }
+    let(:user) { FactoryGirl.create(:user) }
+    let(:mail) { UserMailer.thank_you(user) }
+
 
     it "renders the headers" do
-      expect(mail.subject).to eq("Thank you")
-      expect(mail.to).to eq(["to@example.org"])
+      expect(mail.subject).to eq("Thanks for signing up")
+      expect(mail.to).to eq([user.email])
       expect(mail.from).to eq(["from@example.com"])
     end
 
